9fbc94f9d20a1039e063afd679b66e435124c868
Igor Rozhkov <zawert23879@gmail.com>
2026-03-24 16:40:30 +0300

    хуярю стейт менеджер

---
+++ Assets/Scripts/Unit/StateMachine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StateMachine
+{
+  private Dictionary<Type, State> states = new Dictionary<Type, State>();
+  private State currentState;
+
+  public T GetState<T>() where T : State
+  {
+    states.TryGetValue(typeof(T), out var state);
+    return (T)state;
+  }
+
+  public void AddState(State state)
+  {
+    states[state.GetType()] = state;
+  }
+
+  public void Initialize<T>() where T : State
+  {
+    ChangeState<T>();
+  }
+
+  public void ChangeState<T>() where T : State
+  {
+    Type type = typeof(T);
+
+    if (!states.ContainsKey(type))
+    {
+      UnityEngine.Debug.LogWarning($"State {type} not found in FSM");
+      return;
+    }
+
+    currentState?.Exit();
+    currentState = states[type];
+    currentState.Enter();
+  }
+
+  public void Update()
+  {
+    currentState?.Update();
+  }
+}
---
+++ Assets/Scripts/Unit/States/IdleState.cs
@@ -0,0 +1,10 @@
+﻿public class IdleState : State
+{
+  public IdleState(Unit unit, StateMachine sm) : base(unit, sm)
+  {
+  }
+
+  public override void Update()
+  {
+  }
+}
